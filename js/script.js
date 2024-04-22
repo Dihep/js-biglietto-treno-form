4d980ba62f72bcf4533035ac2eede37fa4197848
Dihep <dihep.demayo@gmail.com>
2024-04-22 20:25:55 +0200

    Basic scripting and testing.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log(`JS is here`);
+const km = document.getElementById("distance");
+console.log(km);
+const age = document.getElementById("age");
+console.log(age);
+const mathButton = document.getElementById("math_button");
+console.log(mathButton);
+function calcPrice() {
+    console.log(`calcPrice`)
+};
