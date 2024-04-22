a60fb18e5b260491397e2fdfb0db05087c882a01
Dihep <dihep.demayo@gmail.com>
2024-04-22 20:35:47 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-console.log(`JS is here`);
 const km = document.getElementById("distance");
-console.log(km);
 const age = document.getElementById("age");
-console.log(age);
 const mathButton = document.getElementById("math_button");
-console.log(mathButton);
 function calcPrice() {
-    console.log(`calcPrice`)
+    let price = parseInt(km.value)*0.21;
+    if (age.value<18) {
+        price *= 0.8
+    }
+    else if (age.value>=65) {
+        price *= 0.6
+    }
+    console.log(price);
+    document.querySelector(`h1`).innerHTML = price;
 };
