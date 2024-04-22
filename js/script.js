8c0cfab7852702a708c72ca5a6c83d3968db33ef
Dihep <dihep.demayo@gmail.com>
2024-04-22 20:54:20 +0200

    Basic structure.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
-const km = document.getElementById("distance");
-const age = document.getElementById("age");
-const mathButton = document.getElementById("math_button");
-function calcPrice() {
-    let price = parseInt(km.value)*0.21;
-    if (age.value<18) {
-        price *= 0.8
-    }
-    else if (age.value>=65) {
-        price *= 0.6
-    }
-    console.log(price);
-    document.querySelector(`h1`).innerHTML = price;
-};
+const m = document.querySelector("main");
+m.innerHTML = `<div>Test</div>`;
